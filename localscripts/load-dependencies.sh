6b1113793d21a47d332c20dc20f69e43e4be368f
johnko <git@johnko.ca>
2015-05-07 18:33:52 -0400

    groupcache is not needed

--- localscripts/load-dependencies.sh
+++ localscripts/load-dependencies.sh
@@ -4,6 +4,6 @@ export GOPATH=~/opt/go/
 
 go get "github.com/PuerkitoBio/ghost/handlers"
 #go get "github.com/golang/gddo/httputil/header"
-go get "github.com/golang/groupcache"
+#go get "github.com/golang/groupcache"
 go get "github.com/gorilla/mux"
 go get "github.com/kennygrant/sanitize"
